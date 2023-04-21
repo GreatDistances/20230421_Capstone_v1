a81d005943a089bac54f63464dd5e14376a462a9
GreatDistances <117937385+GreatDistances@users.noreply.github.com>
2023-04-21 18:00:03 -0500

    navbar experimentation via w3schools

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -2,37 +2,24 @@ import "./NavBar.css";
 
 const NavBar = (props) => {
   return (
-    <div className="navigation">
-      <a href="/" className="brand-name">
-        MovieDL
-      </a>
-      <button className="hamburger">
-        {/* icon from heroicons.com */}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          viewBox="0 0 20 20"
-          fill="white"
-        >
-          <path
-            fillRule="evenodd"
-            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </button>
-      <div className="navigation-menu">
-        <ul>
-          <li>
-            <a href="/home">Home</a>
-          </li>
-          <li>
-            <a href="/search">Search</a>
-          </li>
-          <li>
-            <a href="/account">My Account</a>
-          </li>
-        </ul>
+    <div className="topnav">
+      <div className="topnavbrand">
+        <a href="/index">MovieDL</a>
+      </div>
+      <div className="topnav-right">
+        <a href="/search" className="topnav-right">
+          Search
+        </a>
+        <a href="/login" className="topnav-right">
+          Log In
+        </a>
+        <a href="/account" className="topnav-right">
+          My Account
+        </a>
+        <button className="topnav-right">Genre Select</button>
+        <a href="/cart" className="topnav-right">
+          Cart
+        </a>
       </div>
     </div>
   );
